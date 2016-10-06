309300dc8af21a24a391ffc2f8b634a1187d0d48
Cheechyo <kwj9211@gmail.com>
2016-10-06 22:29:53 +0900

    sh 파일로 실행하게끔 함

--- compiler/requestHandlers.js
+++ compiler/requestHandlers.js
@@ -5,6 +5,13 @@ var sync = require('sync');
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
+  var source = 
+    '#!/bin/python3' + '\n' + 
+    'import sys' + '\n' + 
+    'n = int(input().strip())' + '\n' + 
+    'arr = input().strip().split(" ")' + '\n' + 
+    'arr.reverse()' + '\n' + 
+    'print(" ".join(arr), end="")';
   var body = 
     '<html>'+
     '<head>'+
@@ -13,11 +20,12 @@ function start(response, postData) {
     '</head>'+
     '<body>'+
     '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<textarea name="text" rows="20" cols="60" >' + source + '</textarea>'+
     '<input type="submit" value="Submit" />'+
     '</form>'+
     '</body>'+
     '</html>';
+
 
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
@@ -27,24 +35,12 @@ function start(response, postData) {
 function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent the text(postData): "
-    + querystring.parse(postData).text + "\n")
-  exec("python3 source.py < input00.txt > result00.txt 2> error00.txt; (diff result00.txt output00.txt > tmp00 && echo 00PASS) || echo 00FAIL && rm tmp00", function(error, stdout, stderr){
+  response.write("You've sent the text(postData)\n"
+    + querystring.parse(postData).text + "\n\n")
+  exec("./judge.sh", function(error, stdout, stderr){
     response.write(stdout);
-    exec("python3 source.py < input01.txt > result01.txt 2> error01.txt; (diff result01.txt output01.txt > tmp01 && echo 01PASS) || echo 01FAIL && rm tmp01", function(error, stdout, stderr){
-      response.write(stdout);
-      exec("python3 source.py < input02.txt > result02.txt 2> error02.txt; (diff result02.txt output02.txt > tmp02 && echo 02PASS) || echo 02FAIL && rm tmp02", function(error, stdout, stderr){
-        response.write(stdout);
-        response.end();
-      });
-    });
+    response.end();
   });
-  
-  /*
-  exec("python3 source.py < input00.txt > result00.txt >> error00.txt; (diff result00.txt output00.txt > tmp00 && echo 00PASS) || echo 00FAIL && rm tmp00", callback_exec);
-  exec("python3 source.py < input01.txt > result01.txt >> error01.txt; (diff result01.txt output01.txt > tmp01 && echo 01PASS) || echo 01FAIL && rm tmp01", callback_exec);
-  exec("python3 source.py < input02.txt > result02.txt >> error02.txt; (diff result02.txt output02.txt > tmp02 && echo 02PASS) || echo 02FAIL && rm tmp02", callback_exec);
-  */
 }
 
 exports.start = start;
