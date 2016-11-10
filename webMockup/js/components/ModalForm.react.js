b672eea8eb16c04d10f70b4c601553f57dce3f58
Cheechyo <kwj9211@gmail.com>
2016-11-10 09:15:23 +0900

    문구 수정

--- webMockup/js/components/ModalForm.react.js
+++ webMockup/js/components/ModalForm.react.js
@@ -30,7 +30,7 @@ class ModalForm extends Component {
             &nbsp; <b>Test Case #0 &nbsp; &nbsp; &nbsp; &nbsp; Pass</b>
           </Modal.Content>
           <Modal.Actions>
-          <Button id="btn" onClick={function(){window.open("http://localhost:8003/iframe-embed.html#code=a_var+%3D+'global+value'%0Adef+outer(%29%3A%0A++a_var+%3D+'enclosed+value'%0A%0A++def+inner(%29%3A%0A++++a_var+%3D+'local+value'%0A++++print(a_var%29%0A++++def+inner2(%29%3A%0A++++++++a_var+%3D+'local+value2'%0A++++++++print(a_var%29%0A++++inner2(%29%0A++inner(%29%0A%0Aouter(%29&mode=display&origin=&cumulative=false&heapPrimitives=&textReferences=&py=2&rawInputLstJSON=%5B%5D&curInstr=0")}}>No</Button>
+          <Button id="btn" onClick={function(){window.open("http://localhost:8003/iframe-embed.html#code=a_var+%3D+'global+value'%0Adef+outer(%29%3A%0A++a_var+%3D+'enclosed+value'%0A%0A++def+inner(%29%3A%0A++++a_var+%3D+'local+value'%0A++++print(a_var%29%0A++++def+inner2(%29%3A%0A++++++++a_var+%3D+'local+value2'%0A++++++++print(a_var%29%0A++++inner2(%29%0A++inner(%29%0A%0Aouter(%29&mode=display&origin=&cumulative=false&heapPrimitives=&textReferences=&py=2&rawInputLstJSON=%5B%5D&curInstr=0")}}>View</Button>
           </Modal.Actions>
           <Modal.Content>
             <Icon color='green' name='users' />
