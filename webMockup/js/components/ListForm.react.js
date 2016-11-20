8354dd9538ec08659242761c58ee7927bcc61c8a
Cheechyo <kwj9211@gmail.com>
2016-11-20 16:31:31 +0900

    입력된 소스코드 시각화

--- webMockup/js/components/ListForm.react.js
+++ webMockup/js/components/ListForm.react.js
@@ -7,83 +7,82 @@ import axios from 'axios';
 import { requestQuestions } from '../actions/AppActions';
 
 let style = {
-    td : {
-        "width": "350px",
-        "padding": "10px",
-        "verticalAlign": "top",
-        "borderBottom": "1px solid #ccc"
-    }
+	td : {
+		"width": "350px",
+		"padding": "10px",
+		"verticalAlign": "top",
+		"borderBottom": "1px solid #ccc"
+	}
 }
 
 class ListForm extends Component {
 
   componentDidMount() {
-      let tryAjax = () => {
-          axios.get('/api/questions').then(response => {
-              this.setState({value:response.data});
-          });
-      }
+	  let tryAjax = () => {
+		axios.get('/api/questions').then(response => {
+			this.setState({value:response.data});
+		});
+	  }
 
-      tryAjax();
+	  tryAjax();
 
   }
 
   render() {
-    if (this.state && this.state.value){
-        var rtn = this.state.value.map(function(object, i){
-            return <Row obj={object} key={i} />;
-        });
-        return (
-            <tbody id="list_form" style={{borderBottom:"1px solid #ccc"}}>
-                {rtn}
-            </tbody>
-        );
-    } else {
-        return (
-            <tbody id="list_form">
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        );
-    }
+	if (this.state && this.state.value){
+		var rtn = this.state.value.map(function(object, i){
+			return <Row obj={object} key={i} />;
+		});
+		return (
+			<tbody id="list_form" style={{borderBottom:"1px solid #ccc"}}>
+				{rtn}
+			</tbody>
+		);
+	} else {
+		return (
+			<tbody id="list_form">
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</tbody>
+		);
+	}
   }
 }
 class Row extends Component {
-    render () {
-        var qLink = "/question/" + this.props.obj.id;
-        return (
-            <tr>
-                <td></td>
-                <td>{this.props.obj.id}</td>
-                <td style={{textAlign: "left"}}><Link to={qLink}>{this.props.obj.name}</Link></td>
-                <td>{this.props.obj.author}</td>
-                <td>{this.props.obj.count}</td>
-                <td></td>
-            </tr>
-        )
-    }
+	render () {
+		var qLink = "/question/" + this.props.obj.id;
+		return (
+			<tr>
+				<td></td>
+				<td>{this.props.obj.id}</td>
+				<td style={{textAlign: "left"}}><Link to={qLink}>{this.props.obj.name}</Link></td>
+				<td>{this.props.obj.author}</td>
+				<td>{this.props.obj.count}</td>
+				<td></td>
+			</tr>
+		)
+	}
 }
 
 const mapStateToProps = (state) => {
-    return {
-        value: state.value
-    };
+	return {
+		value: state.value
+	};
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-    return {
-        onReceive: (value) => {
-            console.log('a');
-            dispatch(requestQuestions(value));
-            console.log('b');
-        }
-    }
+	return {
+		onReceive: (value) => {
+			// TODO ???
+			dispatch(requestQuestions(value));
+		}
+	}
 }
 
 
--- webMockup/js/components/VisualForm.react.js
+++ webMockup/js/components/VisualForm.react.js
@@ -4,37 +4,86 @@ import { Link } from 'react-router';
 import Iframe from 'react-iframe';
 
 function getIp() {
-    var ip = location.hostname;
-    return ip;
+		var ip = location.hostname;
+		return ip;
 }
 
 class VisualFrame extends Iframe {
-    render() {
-        return React.createElement("iframe", {
-            ref: "iframe",
-            frameBorder: "0",
-            src: this.props.url,
-            style: {
-                position: this.props.position,
-                height: this.props.height,
-                width: this.props.width
-            },
-            height: this.props.height,
-            width: this.props.width
-        })
-    }
+		render() {
+				return React.createElement("iframe", {
+						ref: "iframe",
+						frameBorder: "0",
+						src: this.props.url,
+						style: {
+								position: this.props.position,
+								height: this.props.height,
+								width: this.props.width
+						},
+						height: this.props.height,
+						width: this.props.width
+				})
+		}
 }
 
 class VisualForm extends Component {
+	componentWillMount() {
+		// this.appendScript("http://" + getIp() + ":8003" + "/js/jquery-1.8.2.min.js");
+		// this.appendScript("http://" + getIp() + ":8003" + "/js/jquery.ba-bbq.min.js");
+	}
+	appendScript(src){
+		let script = document.createElement("script");
+		script.src = src;
+		script.async = true;
+		document.getElementsByTagName('head')[0].appendChild(script);
+	}
+	render() {
+		let obj = JSON.parse(decodeURIComponent(this.props.source));
+		window.obj = obj;
+		var url = "http://" + getIp() + ":8003" + genEmbedUrlStr(obj.source);
+		console.log(url);
+		//var url = "http://"+ getIp() +":8003/iframe-embed.html#code=a_var+%3D+'global+value'%0Adef+outer(%29%3A%0A++a_var+%3D+'enclosed+value'%0A%0A++def+inner(%29%3A%0A++++a_var+%3D+'local+value'%0A++++print(a_var%29%0A++++def+inner2(%29%3A%0A++++++++a_var+%3D+'local+value2'%0A++++++++print(a_var%29%0A++++inner2(%29%0A++inner(%29%0A%0Aouter(%29&mode=display&origin=&cumulative=false&heapPrimitives=&textReferences=&py=2&rawInputLstJSON=%5B%5D&curInstr=0";
+		return (
+				<VisualFrame url={url}/>
+		)
+	}
+}
 
-  render() {
-    let obj = JSON.parse(decodeURIComponent(this.props.source));
-    window.obj = obj;
-    var url = "http://"+ getIp() +":8003/iframe-embed.html#code=a_var+%3D+'global+value'%0Adef+outer(%29%3A%0A++a_var+%3D+'enclosed+value'%0A%0A++def+inner(%29%3A%0A++++a_var+%3D+'local+value'%0A++++print(a_var%29%0A++++def+inner2(%29%3A%0A++++++++a_var+%3D+'local+value2'%0A++++++++print(a_var%29%0A++++inner2(%29%0A++inner(%29%0A%0Aouter(%29&mode=display&origin=&cumulative=false&heapPrimitives=&textReferences=&py=2&rawInputLstJSON=%5B%5D&curInstr=0";
-    return (
-        <VisualFrame url={url}/>
-    )
-  }
+
+// visualizer를 위한 state를 만드는 함수
+function genEmbedUrlStr(source){
+	// visualizer를 위한 state를 만드는 함수 (genEmbedUrlStr)
+	let getAppState = function(txt) {
+		var ret = {code: txt,
+				mode: 'display', //appMode,
+				origin: undefined, //originFrontendJsFile,
+				cumulative: false,//$('#cumulativeModeSelector').val(),
+				heapPrimitives: undefined, // $('#heapPrimitivesSelector').val(),
+				textReferences: undefined, //$('#textualMemoryLabelsSelector').val(),
+				py: '2', //$('#pythonVersionSelector').val(),
+				/* ALWAYS JSON serialize rawInputLst, even if it's empty! */
+				rawInputLstJSON: '[]', //JSON.stringify(rawInputLst),
+				curInstr: 0//myVisualizer ? myVisualizer.curInstr : undefined
+			};
+
+		// keep this really clean by avoiding undefined values
+		if (ret.cumulative === undefined)
+			delete ret.cumulative;
+		if (ret.heapPrimitives === undefined)
+			delete ret.heapPrimitives;
+		if (ret.textReferences === undefined)
+			delete ret.textReferences;
+		if (ret.py === undefined)
+			delete ret.py;
+		if (ret.rawInputLstJSON === undefined)
+			delete ret.rawInputLstJSON;
+		if (ret.curInstr === undefined)
+			delete ret.curInstr;
+		return ret;
+	}
+	let state = getAppState(source);
+	let embedUrlStr = $.param.fragment("/iframe-embed.html", state, 2 /* clobber all */);
+	embedUrlStr = embedUrlStr.replace(/\)/g, '%29') // replace ) with %29 so that links embed well in Markdown
+	return embedUrlStr;
 }
 
 
